5b9796632530e54f184d630103ef3235873315f3
aatlas0 <abdellatibougataya.prt8@gmail.com>
2026-03-01 14:50:46 +0000

    Fix Vercel build error by removing redundant viteStaticCopy

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
-        react(),
-        viteStaticCopy({
-            targets: [
-                {
-                    src: 'coconut-milk-glow/dist/*',
-                    dest: 'coconut-milk-glow'
-                },
-                {
-                    src: 'sea-minerals/dist/*',
-                    dest: 'sea-minerals'
-                },
-                {
-                    src: 'shea-butter-rich/dist/*',
-                    dest: 'shea-butter-rich'
-                },
-                {
-                    src: 'shine-awapuhi/dist/*',
-                    dest: 'shine-awapuhi'
-                }
-            ]
-        })
+        react()
     ],
 })
